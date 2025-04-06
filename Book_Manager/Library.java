459e7d6f730b5c33ec8f557925ac469bf4c5ab51
Te Mihinga <164105382+forbesballardte@users.noreply.github.com>
2025-04-07 09:22:45 +1200

    Started Project and made the book class

---
+++ Book_Manager/Library.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import ecs100.*;
+/**
+ * Write a description of class Library here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Library
+{
+    // instance variables
+    private HashMap<Integer, Book> library;
+    private int currBookId;
+    private Book currBook;
+
+    /**
+     * Constructor for objects of class Library
+     */
+    public Library()
+    {
+        // initialise instance variables
+        
+    }
+
+}
