71d560482e6449d36b4c9049d60fa17446b5848e
Te Mihinga <164105382+forbesballardte@users.noreply.github.com>
2025-04-07 09:43:58 +1200

    Started on the library and its methods

--- Book_Manager/Library.java
+++ Book_Manager/Library.java
@@ -19,7 +19,44 @@ public class Library
     public Library()
     {
         // initialise instance variables
+        library = new HashMap<Integer, Book>(); // initialise HashMap
         
+        // create books
+        Book b1 = new Book(1, "The Wicked King", "Holly Black", 2);
+        Book b2 = new Book(2, "Harry Potter", "J.K Rowling", 4);
+        Book b3 = new Book(3, "1984", "George Orwell", 6);
+        
+        // add books to collection
+        library.put(1, b1);
+        library.put(2, b2);
+        library.put(3, b3);
+        
+        this.currBookId = 3;
     }
-
+    
+    public void menu()
+    {
+        String choice;
+        do {
+            UI.println("(A)dd a Book");
+            UI.println("(F)ind a Book");
+            UI.println("(P)rint all Books");
+            UI.println("(Q)uit");
+            
+            choice = UI.askString("Enter a Choise: ");
+            
+            if (choice.equalsIgnoreCase("A")) {
+                // add books
+            } else if (choice.equalsIgnoreCase("F")) {
+                // find a book
+            } else if (choice.equalsIgnoreCase("P")) {
+                // Print all books
+            } else if (choice.equalsIgnoreCase("Q")) {
+                UI.println("Goodbye");
+                UI.quit();
+            } else {
+                UI.println("Not an Option");
+            }
+        } while (!choice.equalsIgnoreCase("Q"));
+    }
 }
